f4fb5d35a766ada57e09f77740fba784c322c2b8
Jack Skupien <jskupien@vols.utk.edu>
2024-11-18 14:08:48 -0500

    Working on ios navigation menu

---
+++ watch_v5/WelcomeScreen.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  watch_v5
+//
+//  Created by Jack Skupien on 9/20/24.
+//
+
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        VStack {//vertical stack container
+            //add globe image
+            Image("flag")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+//                .frame(width: 100.0, height: 100.0)
+//                .resizable()
+                //.imageScale(.large)
+                .foregroundStyle(.tint)
+            //add text
+            Text("Hello, Diver!")
+                .bold()
+                .font(.system(size: 32))
+                .frame(width: 200)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WelcomeScreen()
+}
