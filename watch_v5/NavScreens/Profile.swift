a5eb4c55e0e7013ecdfbb431d8c9a6a79857fd4b
Jack Skupien <jskupien@vols.utk.edu>
2025-02-03 12:01:03 -0500

    Profile tab

--- watch_v5/NavScreens/Profile.swift
+++ watch_v5/NavScreens/Profile.swift
@@ -16,13 +16,28 @@ struct ProfileView: View {
 //                    .font(.largeTitle)
 //                    .padding()
                 
-                //add profile picture
-                Image("ProfilePicture")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(.tint)
-                
+                HStack{
+                    //profile picture
+                    Image("ProfilePicture")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120, height: 120)
+                        .foregroundStyle(.tint)
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom:10, trailing: 5))
+                    
+//                    Spacer()
+                    //profile info
+                    VStack(alignment: .leading){
+                        Text("Jack Skupien").font(.system(size:28, weight: .semibold))
+                        Text("Master Diver (Candidate)").font(.system(size:23, weight: .light))
+                            //.foregroundColor(.accentColor)
+                        Text("Rescue Diver").font(.system(size:18, weight: .light))
+                        Text("Advanced Diver").font(.system(size:18, weight: .light))
+                        Text("Open Water Diver").font(.system(size:18, weight: .light))
+                    }//.padding()
+                }
+                Spacer()
+                Text("[Profile Content Placeholder]").foregroundColor(.accentColor)
                 Spacer()
             }
             .navigationTitle("Profile")
