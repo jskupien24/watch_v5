b74e0b86cf567d3dca7afc2ad11eed3a7351227b
Jack Skupien <jskupien@vols.utk.edu>
2025-04-04 09:49:33 -0400

    fixed compass2... this thing is looking awesome..

--- watch_v5 Watch App/NavScreens/DiveContainer/compass/compass2.swift
+++ watch_v5 Watch App/NavScreens/DiveContainer/compass/compass2.swift
@@ -1,7 +1,7 @@
 //
 //  Untitled.swift
 //  watch_v5
-//  SEE LINE 105 BEFORE BUILDING-- THE CANVAS'S Y OFFSET IS NOT THE SAME AS THE WATCH'S Y OFFSET
+//  SEE LINE 14 BEFORE BUILDING OR TESTING-- THE CANVAS'S Y OFFSET IS NOT THE SAME AS THE WATCH'S Y OFFSET
 //  Created by Jack Skupien on 4/03/25 (adapted from 3/03/25 version).
 //
 
@@ -11,10 +11,16 @@ struct CompassView2: View {
     @StateObject private var compass = CompassManager()
     
     var body: some View {
+        let t=true//set to true if testing, false if building
         ZStack {
-//            Color.white
-//                .edgesIgnoringSafeArea(.all)
-            CompassNeedle2(angle: compass.heading)
+            //uncomment below block to see white edge safespace
+            /*
+            if(t){
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
+            }
+             */
+            CompassNeedle2(angle: compass.heading,testing: t)
                 .edgesIgnoringSafeArea(.all)
                 .frame(width: .infinity, height: .infinity)
     //            .ignoresSafeArea()
@@ -25,16 +31,24 @@ struct CompassView2: View {
 
 struct CompassNeedle2: View {
     let angle: Double
+    let testing: Bool
     
     var body: some View {
+        //testing bool
+        let vOffset=(testing ? -8.25 : -3.5)
+        //direction line constants
         let lineWidth=2.0
         let lineHeight=300.0
+        //center cover constants
+        let frameWidth=176.5//previously 183.0
+        let frameHeight=222.0
+        let frameCRadius=41.0//previously 40.0
         ZStack {
             //octal directions
             //NNE/SSW line
             Rectangle()
                 .fill(Color.accent.opacity(0.5))
-                .frame(width: lineWidth-1, height: 300)
+                .frame(width: lineWidth-1, height: lineHeight)
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*(angle+22.5)))
             //ESE/WNW line
@@ -46,7 +60,7 @@ struct CompassNeedle2: View {
             //ESE/WSW line
             Rectangle()
                 .fill(Color.accent.opacity(0.5))
-                .frame(width: lineWidth-1, height: 300)
+                .frame(width: lineWidth-1, height: lineHeight)
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*(angle+157.5)))
             //SSE/NNE line
@@ -56,16 +70,16 @@ struct CompassNeedle2: View {
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*(angle+67.5)))
             //rectangle covering octals
-            RoundedRectangle(cornerRadius: 52)
+            RoundedRectangle(cornerRadius: frameCRadius+12)
                 .fill(.black)
 //                .stroke(Color.accent.opacity(0.25), lineWidth: 1)
-                .frame(width: 205, height: 244)
+                .frame(width: frameWidth+22, height: frameHeight+22)
             
             //quadrantal directions
             //NE/SW line
             Rectangle()
                 .fill(Color.accent.opacity(0.5))
-                .frame(width: lineWidth-0.5, height: 300)
+                .frame(width: lineWidth-0.5, height: lineHeight)
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*(angle+45)))
             //SE/NW line
@@ -75,17 +89,17 @@ struct CompassNeedle2: View {
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*(angle+135)))
             //rectangle covering cardinals
-            RoundedRectangle(cornerRadius: 45)
+            RoundedRectangle(cornerRadius: frameCRadius+5)
                 .fill(.black)
 //                .stroke(Color.accent.opacity(0.25), lineWidth: 1)
-                .frame(width: 194, height: 233)
+                .frame(width: frameWidth+11, height: frameHeight+11)
             
             
             //cardinal directions
             //North and south line
             Rectangle()
                 .fill(Color.accent.opacity(1))
-                .frame(width: lineWidth, height: 300)
+                .frame(width: lineWidth, height: lineHeight)
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*angle))
             //east and west line
@@ -95,17 +109,16 @@ struct CompassNeedle2: View {
                 .offset(y:0)
                 .rotationEffect(.degrees(-1*angle+90))
             //rectangle covering cardinals
-            RoundedRectangle(cornerRadius: 40)
+            RoundedRectangle(cornerRadius: frameCRadius)
                 .fill(.black)
 //                .stroke(Color.accent.opacity(0.25), lineWidth: 1)
-                .frame(width: 183, height: 222)
+                .frame(width: frameWidth, height: frameHeight)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center) // Extend fully
-        .offset(y:-8.5)
-//        .offset(y:-3.5)///////uncomment when building to watch
+        .offset(y:vOffset)
         .edgesIgnoringSafeArea(.all)
 
-            .clipped()
+//            .clipped()
         .navigationBarBackButtonHidden(true)
     }
 }
