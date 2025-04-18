fe8829754613e58f3a82ef6efe5800ffe52f8399
Jack Skupien <jskupien@vols.utk.edu>
2025-04-18 17:03:20 -0500

    dark mode discover page fixed

---
+++ watch_v5/Managers/LocationManager.swift
@@ -0,0 +1,24 @@
+/* class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+    @Published var userLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() // Ask for permission
+        locationManager.startUpdatingLocation() // Start location updates
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            DispatchQueue.main.async {
+                self.userLocation = location.coordinate
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get location: \(error.localizedDescription)")
+    }
+}
+*/
